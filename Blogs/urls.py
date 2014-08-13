dc48f5f2262dc1151e26f4033ec891070c4dcd9b
Ali ünsal <aliunsal@live.com>
2014-08-13 14:43:30 +0300

    singel_post icin name degisti

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, url
-
 from Blogs import views
 
 urlpatterns = patterns('',
-
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_id>[0-9])/$', views.post, name='post'),
+    url(r'(?P<post_id>[0-9])/$', views.post, name='single_post'),
 )
