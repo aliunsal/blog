28ddc983a3b25cebee5715e0f549502ff280b948
Ali ünsal <aliunsal@live.com>
2014-08-15 11:14:09 +0300

    Kullanicilar icin email girisi

---
+++ Users/backends.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailAuthBackend(ModelBackend):
+
+    def authenticate(self, email=None, password=None, **kwargs):
+        # Some authenticators expect to authenticate by 'username'
+        if email is None:
+            email = kwargs.get('username')
+
+        try:
+            user = User.objects.get(email=email, is_active=True)
+            if user.check_password(password):
+                user.backend = "%s.%s" % (self.__module__, self.__class__.__name__)
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
