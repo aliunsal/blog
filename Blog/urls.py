2b0c0cf1eb4978bde183077294d5213d5316e828
Ali ünsal <aliunsal@live.com>
2014-08-20 14:10:00 +0300

    blog index sayfalama icin url eklenedi

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'Blogs.views.index', name='index'),
+    url(r'^(?P<page_index>[0-9]+)/', 'Blogs.views.index', name='index_pager'),
     url(r'^blog/', include('Blogs.urls')),
     url(r'^user/', include('Users.urls')),
     url(r'^admin/', include(admin.site.urls)),
