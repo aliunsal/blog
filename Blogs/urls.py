c0eff5615f3f497179570507dc769d3df0156b65
Ali ünsal <aliunsal@live.com>
2014-08-20 14:08:30 +0300

    url in '9' dan buyuk rakamlari alabilmesi duzeltildi

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from Blogs import views
 
 urlpatterns = patterns('',
-    url(r'(?P<post_id>[0-9])/$', views.post, name='single_post'),
+    url(r'(?P<post_id>[0-9]+)/$', views.post, name='single_post'),
 )
