6ed3948ed0880ef339ce54c668a9167e43b55c1e
Ali ünsal <aliunsal@live.com>
2014-08-25 09:44:50 +0300

    user modulu linkleri eklendi ve degisiklikler yapildi

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url, patterns
 from Users import views
 
+
+
 urlpatterns = patterns('Users',
     url(r'^$', views.index, name="user_index"),
-    url(r'^login/', views.UserLogin, name='user_login'),
-    url(r'^logout/', views.UserLogout, name='user_logout'),
+    url(r'^login/', views.user_login, name='user_login'),
+    url(r'^logout/', views.user_logout, name='user_logout'),
+    url(r'^posts/remove/(?P<post_id>[0-9]+)', views.user_post_remove, name='user_post_remove'),
+    url(r'^posts/(?P<post_id>[0-9]+)', views.user_post_edit, name='user_post_edit'),
+    url(r'^posts/add', views.user_post_add, name='user_post_add'),
+    url(r'^posts/', views.user_post_list, name='user_post_list'),
+    url(r'^profile/', views.user_profile, name='user_profile'),
+    url(r'^profile/', views.register, name='user_profile'),
 )
