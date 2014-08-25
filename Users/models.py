f594fe77067dc90369752006514eb62ed626e8b4
Ali ünsal <aliunsal@live.com>
2014-08-25 09:43:27 +0300

    user profile modeli eklendi

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField(upload_to="/img/user_avatar/", null=True)
+    activation_key = models.TextField(null=True)
+    key_expiration_date = models.DateTimeField(null=True)
