23f9e4846b2549b1ff6dd3c3863bc70e43e4365d
Ali ünsal <aliunsal@live.com>
2014-08-25 09:40:46 +0300

    blog icin index viewi ve sayfalamas yazıldı, email actication olusturuldu, singel post sayfasi icin yorum formlarini ayarlandi

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,35 +1,56 @@
-from django.shortcuts import render, render_to_response
-from Blogs.models import Post
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from Blogs.models import Post, Comment
 from Blogs.forms import AnonymousUserCommentForm, UserCommentForm
-from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 
+def index(request, **kwargs):
+    start_index = 0
+    last_index = 3
+    context = {}
+    if 'page_index' in kwargs:
+        start_index = last_index * int(kwargs["page_index"])
+        last_index += start_index
+        context["page_index"] = kwargs["page_index"]
+        context["prev"] = int(kwargs["page_index"]) - 1
+        context["next"] = int(kwargs["page_index"]) + 1
+    else:
+        context["prev"] = 0
+        context["next"] = 1
+    post_list = Post.objects.all().order_by("-date")[start_index:last_index]
+    context['post_list'] = post_list
 
-def index(request):
-    post_list = Post.objects.all().order_by("-date")[:10]
-    context = {'post_list': post_list}
     return render(request, 'Blogs/index.html', context)
 
 
 def post(request, post_id):
     single_post = Post.objects.get(id=post_id)
-    content_type = ContentType.objects.get_for_model(single_post)
-    form = ""
-    if request.method == "POST":
-        if request.user.is_authenticated():
-            form = UserCommentForm(request.POST, initial={"email": request.user.email,
-                                                          "author": request.user,
-                                                          "object_id": post_id
-                                                          })
-            form.has_changed()
-        else:
-            form = AnonymousUserCommentForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-
+    comments = single_post.comment.filter(approved=True)
+    if request.user.is_authenticated():
+        form = UserCommentForm()
+        if request.POST:
+            form = UserCommentForm(request.POST)
+            if form.is_valid():
+                form.save(user=request.user, post=single_post)
+            else:
+                return render(request, 'Blogs/single_post.html', {"post": single_post,
+                                                                  "form": form,
+                                                                  "comments": comments})
     else:
-        if request.user.is_authenticated():
-            form = UserCommentForm
-        else:
-            form = AnonymousUserCommentForm
-    return render(request, 'Blogs/single_post.html', {'post': single_post, 'form': form})
+        form = AnonymousUserCommentForm()
+        if request.POST:
+            form = AnonymousUserCommentForm(request.POST)
+            if form.is_valid():
+                form.save(post=single_post)
+            else:
+                return render(request, 'Blogs/single_post.html', {"post": single_post,
+                                                                  "form": form,
+                                                                  "comments": comments})
+    return render(request, 'Blogs/single_post.html', {"post": single_post,
+                                                      "form": form,
+                                                      "comments": comments})
+
+
+def email_activation(request, key):
+    Comment.objects.filter(activation_key=key).update(approved=True)
+    return HttpResponseRedirect(reverse("index"))
