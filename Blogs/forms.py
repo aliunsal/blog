9ad97c543f1b11044f7b14f1563baebc07d6bf7c
Ali ünsal <aliunsal@live.com>
2014-08-25 09:20:24 +0300

    yorum formlari eklendi ve 'save' mehtodlari yeniden yazildi. Anonim kullanici yorum ekledikten sonra mail atilmasi saglandi

--- Blogs/forms.py
+++ Blogs/forms.py
@@ -1,12 +1,38 @@
+import random
 from django import forms
+from django.core.urlresolvers import reverse
 from Blogs.models import Comment
+from django.utils import timezone
+from django.contrib.auth.models import User
+import task
+import uuid
+import hashlib
 
 
 class AnonymousUserCommentForm(forms.ModelForm):
 
+    name = forms.CharField(max_length=100)
+
     class Meta:
         model = Comment
-        fields = ['email', 'content']
+        fields = ['name', 'email', 'content']
+
+    def save(self, post):
+        user = User.objects.get(email=self.cleaned_data["email"])
+        if not user:
+            user = User.objects.create_user(username=uuid.uuid4(), email=self.cleaned_data["email"])
+
+        comment = Comment()
+        comment.content = self.cleaned_data["content"]
+        comment.author = user
+        comment.date = timezone.now()
+        comment.approved = False
+        comment.email = user.email
+        comment.content_object = post
+        comment.activation_key = hashlib.sha256(user.email + str(random.random())).hexdigest()
+        comment.save()
+
+        task.mail_send.delay("Email Activation", reverse("email", args=(comment.activation_key,)), None, [comment.email])
 
 
 class UserCommentForm(forms.ModelForm):
@@ -14,3 +40,14 @@ class UserCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+    def save(self, post, user):
+        comment = Comment()
+        comment.content = self.cleaned_data["content"]
+        comment.author = user
+        comment.date = timezone.now()
+        comment.approved = True
+        comment.email = user.email
+        comment.content_object = post
+        comment.activation_key = "0"
+        comment.save()
