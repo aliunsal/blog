158b0ddabf2b89864b0e97565df9ffb6d33b1895
Ali ünsal <aliunsal@live.com>
2014-08-27 12:52:50 +0300

    sub comment eklenmesi icin fonksiyon yazildi

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,8 +1,17 @@
+import hashlib
+import random
+from time import timezone
+import uuid
+from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from Blogs.models import Post, Comment
 from Blogs.forms import AnonymousUserCommentForm, UserCommentForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+import task
+
 
 def index(request, **kwargs):
     start_index = 0
@@ -24,7 +33,12 @@ def index(request, **kwargs):
 
 
 def post(request, post_id):
-    single_post = Post.objects.get(id=post_id)
+    temp_post = cache.get(post_id)
+    if temp_post:
+        single_post = temp_post
+    else:
+        single_post = Post.objects.get(id=post_id)
+        cache.set(post_id, single_post, timeout=None)
     comments = single_post.comment.filter(approved=True)
     if request.user.is_authenticated():
         form = UserCommentForm()
@@ -35,7 +49,8 @@ def post(request, post_id):
             else:
                 return render(request, 'Blogs/single_post.html', {"post": single_post,
                                                                   "form": form,
-                                                                  "comments": comments})
+                                                                  "comments": comments,
+                                                                  "request": request})
     else:
         form = AnonymousUserCommentForm()
         if request.POST:
@@ -45,10 +60,46 @@ def post(request, post_id):
             else:
                 return render(request, 'Blogs/single_post.html', {"post": single_post,
                                                                   "form": form,
-                                                                  "comments": comments})
+                                                                  "comments": comments,
+                                                                  "request": request})
     return render(request, 'Blogs/single_post.html', {"post": single_post,
                                                       "form": form,
-                                                      "comments": comments})
+                                                      "comments": comments,
+                                                      "request": request})
+
+
+def sub_comment_add(request, comment_id):
+    if request.user.is_authenticated():
+
+        comment = Comment.objects.get(id=comment_id)
+        new_comment = Comment(content=request.POST["content"],
+                              author=request.user,
+                              approved=True,
+                              activation_key=0,
+                              content_object=comment,
+                              email=request.user.email
+                              )
+        new_comment.save()
+    else:
+        user = User.objects.get(email=request.POST["email"])
+        if not user:
+            user = User.objects.create_user(username=uuid.uuid4(),
+                                            email=request.POST["email"],
+                                            is_active=False)
+
+        comment = Comment()
+        comment.content = request.POST["content"]
+        comment.author = user
+        comment.date = timezone.now()
+        comment.approved = False
+        comment.email = user.email
+        comment.content_object = post
+        comment.activation_key = hashlib.sha256(user.email + str(random.random())).hexdigest()
+        comment.save()
+        content = render_to_string("Email/email.html", {"activation_key": comment.activation_key})
+        task.mail_send.delay("Email Activation", content, None, [comment.email])
+
+    return HttpResponseRedirect(request.GET["next"])
 
 
 def email_activation(request, key):
