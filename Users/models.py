785560abee0d813f2350edca0ea916e4f8d94f43
Ali ünsal <aliunsal@live.com>
2014-08-27 12:55:34 +0300

    user avatar icin defaut deger atandi

--- Users/models.py
+++ Users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    avatar = models.ImageField(upload_to="/img/user_avatar/", null=True)
-    activation_key = models.TextField(null=True)
-    key_expiration_date = models.DateTimeField(null=True)
+    avatar = models.ImageField(upload_to="static/img/user_avatar/",
+                               default="static/img/user_avatar/user.png",
+                               null=True)
+    activation_key = models.TextField(max_length=150, null=True)
+    key_expiration_date = models.DateTimeField(auto_now_add=True, null=True)
