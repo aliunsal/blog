713e6606732b8ce899eb198cbf62fdda0f5bfb34
Ali ünsal <aliunsal@live.com>
2014-08-27 13:05:14 +0300

    user aktivasyon, user profile methodları düzenlendi

--- Users/views.py
+++ Users/views.py
@@ -1,10 +1,12 @@
+import hashlib
+import random
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
-from Users.forms import UserPost, UserProfile as UserProfileForm
+from django.template.loader import render_to_string
+from Users.forms import UserPost, UserProfile as UserProfileForm, UserRegister
 from Users.models import UserProfile
 from Blogs.models import Post
 from django.utils import timezone
@@ -53,9 +55,21 @@ def user_post_edit(request, post_id):
     post = Post.objects.get(id=post_id)
     if post.author != request.user:
         return HttpResponseRedirect(reverse("user_post_list"))
-    form = UserPost(initial={"title": post.title,
-                             "content": post.content,
-                             "picture": post.picture.url})
+    if request.POST:
+        form = UserPost(request.POST, request.FILES)
+        if form.is_valid():
+            post.title = form.cleaned_data["title"]
+            post.content = form.cleaned_data["content"]
+            if form.cleaned_data["picture"] and post.picture != form.cleaned_data["picture"]:
+                post.picture = form.cleaned_data["picture"]
+            post.save()
+            task.resize_image.delay(post.picture.url, 636, 237)
+        else:
+            return render(request, "Users/post_edit.html", {"form": form})
+    else:
+        form = UserPost(initial={"title": post.title,
+                                 "content": post.content,
+                                 "picture": post.picture.url})
     return render(request, "Users/post_edit.html", {"form": form})
 
 
@@ -75,13 +89,13 @@ def user_post_add(request):
                 task.resize_image.delay(post.picture.url, 636, 237)
             except Exception as e:
                 return HttpResponse(str(e))
-
+            return render(request, "Users/post_add.html", {"form": form})
         else:
             return render(request, "Users/post_add.html", {"form": form})
     else:
         form = UserPost()
-        return render(request, "Users/post_add.html", {"form": form})
-    return HttpResponse("hic bisey")
+    return render(request, "Users/post_add.html", {"form": form})
+
 
 
 @login_required()
@@ -99,24 +113,51 @@ def user_profile(request):
         return render(request, "Users/profile.html", {"user": user,
                                                       "form": form})
     else:
-        form = UserProfileForm(request.POST)
+        form = UserProfileForm(request.POST, request.FILES)
         if form.is_valid():
-            #user_profile = UserProfile(user=request.user,avatar=)
-            return HttpResponse("")
-        else:
-            return HttpResponse(form.cleaned_data["email"])
+            user.user.first_name = form.cleaned_data["first_name"]
+            user.user.last_name = form.cleaned_data["last_name"]
 
-    return HttpResponse("hic bisey")
+            if user.user.email != form.cleaned_data["email"]:
+                user.user.email = form.cleaned_data["email"]
+                user.activation_key = hashlib.sha256(user.email + str(random.random())).hexdigest()
+                content = render_to_string("Email/email.html", {"activation_key": user.activation_key})
+                task.mail_send.delay("Email Activation", content, None, [user.user.email])
+                user.user.save()
+            if form.cleaned_data["avatar"] and user.avatar != form.cleaned_data["avatar"]:
+                user.avatar = form.cleaned_data["avatar"]
+
+            try:
+                user.save()
+                user.user.save()
+                task.resize_image.delay(user.avatar.url, 70, 70)
+            except Exception as e:
+                print e
+
+            return render(request, "Users/profile.html", {"user": user,
+                                                          "form": form})
+        else:
+            return render(request, "Users/profile.html", {"user": user,
+                                                          "form": form})
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegister(request.POST)
         if form.is_valid():
             new_user = form.save()
-            return HttpResponseRedirect(reverse("index"))
+            return render(request, "Users/register.html", {'form': form})
     else:
-        form = UserCreationForm()
+        form = UserRegister()
     return render(request, "Users/register.html", {
         'form': form,
     })
+
+
+def user_activation(request,key):
+    user_profile = UserProfile.objects.filter(activation_key=key)
+    if user_profile:
+        user_profile.user.is_active = True
+        user_profile.user.save()
+
+    return HttpResponseRedirect(reverse("index"))
