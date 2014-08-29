869a0ed448a20b6a6c9133c1fb5d6c0ebbbbbd67
Ali ünsal <aliunsal@live.com>
2014-08-29 09:34:50 +0300

    settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.core.urlresolvers import reverse
+import djcelery
 import south
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -22,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'j=+$z$e4nw(-l2o=9^*$_)0k16&)0*b$2%e5+30#)ohy6%r819'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition.
@@ -41,6 +43,7 @@ INSTALLED_APPS = (
     'Blogs',
     'Users',
     'south',
+    'djcelery'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -67,6 +70,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = (
+    'Users.backends.EmailAuthBackend',
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -84,13 +91,40 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = "/home/aliunsal/PycharmProjects/Blog/static"
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/home/aliunsal/PycharmProjects/Blog',
-)
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+LOGIN_URL = reverse("user_login")
+LOGIN_REDIRECT_URL = reverse("user_index")
+
+#celery
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
+djcelery.setup_loader()
+CELERY_IMPORTS = ("task")
+
+
+#redis
+CACHES = {
+    "default": {
+        "BACKEND": "redis_cache.cache.RedisCache",
+        "LOCATION": "127.0.0.1:6379:1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "redis_cache.client.DefaultClient",
+        }
+    }
+}
+
+#email configration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
